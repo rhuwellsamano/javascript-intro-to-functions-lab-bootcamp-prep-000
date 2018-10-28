0930505a1c056e906b7d05cbc3a3913236ffd5b5
Rhuwell Samano <rhuwell.samano@gmail.com>
2018-10-28 03:39:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ function sayHiToGrandma(string) {
   if string().toUpperCase() === uppercase;
   return "YES INDEED!"
 }
+
+function sayHiToGrandma(string){
+if (string === string.toLowerCase()) return "I can't hear you!";
+else if (string === "I love you, Grandma.") return "I love you, too.";
+else if (string === string.toUpperCase()) return "YES INDEED!";
+}
+sayHiToGrandma("hello");
+sayHiToGrandma("HELLO");
+sayHiToGrandma("I love you, Grandma.");
